cfa4b4249963c773a9d97cb04cd5ea49cf7fd78a
tejasrai <tejas.rai.phe17@iitbhu.ac.in>
2021-07-31 10:13:55 +0530

    added a function to create a block and defined four key value pairs in it.

--- blockchain.py
+++ blockchain.py
@@ -10,7 +10,15 @@ class Blockchain :
         #genesis block
         self.create_block(proof = 1, previous_hash = '0')
 
-    
+    def create_block(self, proof, previous_hash) :
+        block = {'index' : len(self.chain)+1,
+                 'timestamp' : str(datetime.datetime.now()),
+                 'proof' :  proof,
+                 'previous_hash' : previous_hash}
+                 
+                     
+
+
 
 
 
