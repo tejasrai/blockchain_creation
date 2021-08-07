730d2a6889de2d3d119eb0252ab2c93174b7d216
tejasrai <tejas.rai.phe17@iitbhu.ac.in>
2021-08-07 09:45:29 +0530

    added functions for creating blockchain and mining it

--- blockchain.py
+++ blockchain.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify
 from flask.json import htmlsafe_dumps
 
 #building the blockchain
+
 class Blockchain :
     def __init__(self) :
         self.chain = []
@@ -63,6 +64,16 @@ class Blockchain :
 app = Flask(__name__)
 
 #creating a Blockchain
+blockchain = Blockchain()
+
+#mining a new block
+@app.route('/mine_block', methods=['GET'])
+def mine_block() :
+    previous_block = blockchain.get_previous_block
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
 
 
 
